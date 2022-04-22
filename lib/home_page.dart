4f7110cc9ffc35b3ff353521aad03164e449492c
Wes K <91550684+WeslayneK@users.noreply.github.com>
2022-04-21 22:08:58 -0400

    App 1.1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,10 +12,14 @@ class HomePageState extends State<HomePage> {
   int counter = 0; //contador para o número de vezes
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar( //colocando uma barra com logo
+        title: Text(
+          'App ADS',
+        )),
+      body: Center( //corpo
         child: GestureDetector(
-          child: Text('Click: $counter'),
+          child: Text('Click: $counter', style: TextStyle(fontSize: 50)),
           onTap: (){
             setState(() {
               counter++;
@@ -23,6 +27,14 @@ class HomePageState extends State<HomePage> {
             });
           },
         )//pressionar na tela, lado direito, esquerdo
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_circle),
+        onPressed: (){
+          setState(() {
+              counter--;
+            });
+        },
       ),
     );
   }
